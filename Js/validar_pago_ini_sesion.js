63471e1a133600aead16a8549d9157d9c693a609
Nico <nic.roca@duocuc.cl>
2023-05-08 04:51:02 -0400

    Rutas mejor hechas

---
+++ Js/validar_pago_ini_sesion.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("compra").addEventListener('submit', function(evento) {
+      evento.preventDefault();
+      validar_nom_tarjeta();
+      redireccionar();
+    });
+  });
+  
+  function validar_nom_tarjeta() {
+    var nombre_tarjeta = document.getElementById('nom_tarjeta').value;
+    if (nombre_tarjeta.length == 0) {
+      alert('Debe ingresar un nombre para la tarjeta');
+      return false;
+    }
+    return true;
+  } 
+  
+  function redireccionar() {
+    window.location.href = "pago_realizado.html";
+  }
+  
