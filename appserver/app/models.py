a70eaa88028cc68823a79cbfa002e2849b3daeb0
Nico <nic.roca@duocuc.cl>
2023-06-28 02:15:17 -0400

    ICreamos al admin y los modelos

--- appserver/app/models.py
+++ appserver/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class producto(models.Model):
+    descripcion=models.CharField(max_length=100)
+    precio=models.IntegerField()
+    tipo=models.CharField(max_length=20)
+    
+    def _str_(self):
+        return self.descripcion
