162ab67a94ba8a4dede309851cf7b38ff9bd0648
Nico <nic.roca@duocuc.cl>
2023-06-28 04:49:46 -0400

    Edicion del usuario Admin

--- appserver/app/admin.py
+++ appserver/app/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import producto
 # Register your models here.
 
-admin.site.register(producto)
+class ProductosAdmin(admin.ModelAdmin):
+    list_display=["descripcion", "precio", "tipo"]
+    list_editable=["precio"]
+    search_fields=["tipo"]
+    list_per_page: 6
+
+admin.site.register(producto, ProductosAdmin)
