be9ba4b71ecdc56beaefd2a607cc9b8799bddfcd
Nico <nic.roca@duocuc.cl>
2023-07-03 17:18:51 -0400

    A2

--- appserver/app/views.py
+++ appserver/app/views.py
@@ -8,6 +8,7 @@ from django.contrib import messages
 from django.core.paginator import Paginator
 from django.http import Http404
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required, permission_required
 
 # Create your views here.
 
@@ -36,6 +37,7 @@ def guantes(request):
 def canilleras(request):
     return render(request, 'app/canilleras.html')
 
+@permission_required('app.add_producto')
 def agregar_producto(request):
     data={
         'form': ProductoForm()
@@ -51,6 +53,7 @@ def agregar_producto(request):
     
     return render(request, 'app/producto/agregar.html', data)
 
+@permission_required('app.view_producto')
 def listar_productos(request):
     productos=producto.objects.all()
     
@@ -68,6 +71,7 @@ def listar_productos(request):
     }
     return render(request, 'app/producto/listar.html', data)
 
+@permission_required('app.change_producto')
 def modificar_producto(request, id):
     productos=get_object_or_404(producto, id=id)
     data={
@@ -84,6 +88,8 @@ def modificar_producto(request, id):
     
     return render(request, 'app/producto/modificar.html', data)
 
+
+@permission_required('app.delete_producto')
 def eliminar_producto(request, id):
     productoeli=get_object_or_404(producto, id=id)
     productoeli.delete()
